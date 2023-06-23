c39a30d7447239ad225f37a2aeb124380f7183c8
moejo-the-jojo <108475765+moejo-the-jojo@users.noreply.github.com>
2023-06-23 17:22:23 +0200

    fixed repeating sound-slowing bug

--- src/App.js
+++ src/App.js
@@ -52,130 +52,53 @@ const drumSoundSrc = [
 const App = () => {
   const [currentClip, setCurrentClip] = React.useState("");
 
-  const playClip = (audio, display) => {
-    const currentClip = document.getElementById(audio);
-    const currentDisplay = document.getElementById("drum-pad" + audio);
-    currentClip.play();
+  const usedKeys = drumSoundSrc.map((a) => a.key);
+
+  let objectPlaying = {};
+  for (let key of usedKeys) {
+    objectPlaying[key] = false;
+  }
+
+  const playClip = (currentAudio, display, key) => {
+    currentAudio.play();
     setCurrentClip(display);
-    currentDisplay.classList.add("activated");
+    document.getElementById("drum-pad" + key).classList.add("activated");
+
+    if (currentAudio.getAttribute("endedListener") !== "true") {
+      currentAudio.setAttribute("endedListener", "true");
+
+      currentAudio.addEventListener("ended", () => {
+        if (objectPlaying[key] === true) {
+          playClip(currentAudio, display, key);
+        }
+      });
+    }
   };
 
   React.useEffect(() => {
-    const usedKeys = drumSoundSrc.map((a) => a.key);
-
-    let objectPlaying = {};
-    for (let key of usedKeys) {
-      objectPlaying[key] = false;
-    }
-
     window.addEventListener("keydown", (event) => {
       const currentKey = event.key.toUpperCase();
-      const currentObject = drumSoundSrc.filter((a) => a.key === currentKey);
 
       if (usedKeys.includes(currentKey)) {
-        document.getElementById(currentKey).loop = true;
-        playClip(currentKey, currentObject[0].name);
-      }
-    });
+        const currentAudio = document.getElementById(currentKey);
+        const currentDisplay = drumSoundSrc.filter(
+          (a) => a.key === currentKey
+        )[0].name;
 
-    window.addEventListener("keyup", (event) => {
-      const currentKey = event.key.toUpperCase();
-      if (usedKeys.includes(currentKey)) {
-        document.getElementById(currentKey).loop = false;
-      }
-    });
-    /*
-      if (usedKeys.includes(currentKey)) {
-       if(document.getElementById(currentKey).loop == false) { 
-        const currentObject = drumSoundSrc.filter((a) => a.key == currentKey);
         objectPlaying[currentKey] = true;
-        const currentAudioPad = document.getElementById(currentKey);
-        currentAudioPad.loop = true;
-        playClip(currentKey, currentObject[0].name);
-        
-        globalThis.personalTik = setInterval(() => {
-          console.time("isItLong?")
-          for(let key in objectPlaying) {
-            if(objectPlaying[key] == false) {
-              document.getElementById(key).loop = false;
-            }
-          }
-          console.timeEnd("isItLong?")
-        }, 100);
-      } else {
-        return;
-      }
+        playClip(currentAudio, currentDisplay, currentKey);
       }
     });
-    */
-    /*
+
     window.addEventListener("keyup", (event) => {
-      const newCurrentKey = event.key.toUpperCase();
-      if (usedKeys.includes(newCurrentKey)) {
-        objectPlaying[newCurrentKey] = false;
-        //clearInterval(globalThis.personalTik);
-      }
-    });
-    */
-    /*
-    window.addEventListener("keydown", (event) => {
-      const audioPad = event.key.toUpperCase();
-      const neededDisplay = drumSoundSrc.filter((a) => a.key == audioPad); // ??????
-      if (usedKeys.includes(audioPad)) {
-        const thisPad = document.getElementById(audioPad);
-        thisPad.loop = true;
-        playClip(audioPad, neededDisplay[0].name)
-      }
-    })
-    */
-
-    /*
-    window.addEventListener("keydown", (event) => {
-      const audiopad = event.key.toUpperCase();
-      const neededDisplay = drumSoundSrc.filter((a) => a.key == audiopad);
-      if (usedKeys.includes(audiopad)) {
-        const thisPad = document.getElementById(audiopad);
-        const fullDuration = thisPad.maxTime;
-        console.log(fullDuration);
-        globalThis.replayAudioClip = setInterval(playClip(audiopad, neededDisplay[0].name), fullDuration);
-        //document.getElementById(audiopad).loop = true;
-        //playClip(audiopad, neededDisplay[0].name);
-      }
-    });    
-    for(let audiopad of usedKeys) {
-      window.addEventListener("keyup", (event) => {
-        if (event.key.toUpperCase() == audiopad) {
-          const myPad = document.getElementById(audiopad);
-          clearInterval(globalThis.replayAudioClip);
-    */
-
-    /*const maxTime = myPad.duration;          
-          if(myPad.currentTime == maxTime) {
-          myPad.loop = false;
-          myPad.pause();
-          myPad.currentTime = 0;
-          }
-        }
-      })  
-    }
-        */
-
-    /*
-    window.addEventListener("keyup", (event) => {
-        const wantedElement = 
-        for (let audiopad of usedKeys) {
-          document.getElementById(audiopad).loop = false;
-        }
-      }
-    )
-    */
-    for (let audiopad of usedKeys) {
-      document.getElementById(audiopad).addEventListener("ended", () => {
+      const currentKey = event.key.toUpperCase();
+      if (usedKeys.includes(currentKey)) {
+        objectPlaying[currentKey] = false;
         document
-          .getElementById("drum-pad" + audiopad)
+          .getElementById("drum-pad" + currentKey)
           .classList.remove("activated");
-      });
-    }
+      }
+    });
   }, []);
 
   return (
